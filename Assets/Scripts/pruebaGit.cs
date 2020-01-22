71edeeb5fe625bd3b360dabb7ded2bbecd26ce54
Antaragorn <Antaragorn@gmail.com>
2020-01-22 15:39:07 +0100

    prueba modificando archivos y funcionamiento con git

---
+++ Assets/Scripts/pruebaGit.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class pruebaGit : MonoBehaviour
+{
+    
+    void Start()
+    {
+        //probando git
+    }
+
+    //holliiiiiii
+    void Update()
+    {
+        
+    }
+}
